eddc08e6366b045f40805cabce3fd389d032968b
김솔비 <pinery11@naver.com>
2021-07-07 11:21:19 +0000

    main.py edited online with Bitbucket

--- main.py
+++ main.py
@@ -2,55 +2,62 @@ import sys
 import os
 
 import qrcode_wifi
+import qr_result
+
 
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 from PyQt5 import QtWidgets
 
-#리소스 파일 사용시 resource_path()사용해서 절대경로로 변경
-def resource_path(relative_path):
-    try:
-        base_path = sys._MEIPASS
-    except Exception:
-        base_path = os.path.abspath(".")
+set_class = uic.loadUiType('set.ui')[0]
+Qrtest_class = uic.loadUiType("camtest.ui")[0]
 
-    return os.path.join(base_path, relative_path)
-
-
-set_class = uic.loadUiType(resource_path('set.ui'))[0]
-Qrtest_class = uic.loadUiType(resource_path("camtest.ui"))[0]
-
-
-class MyWindow(QMainWindow, set_class):
+wificode ='' 
+class MyWindow(QMainWindow, set_class):    
     def __init__(self):
+        
         super().__init__()
-        self.setupUi(self)
+        self.setupUi(self) 
         self.setWindowTitle("Hoseo Unviersity - IR LAB")
 
-        self.qrcode_btn.clicked.connect(self.btn_connect)
+        self.qrcode_btn.clicked.connect(self.btn_connect) 
     
-    def btn_connect(self):
-        wificode = qrcode_wifi.start()
-        print(wificode)
-
-class Set_Window(QMainWindow, Qrtest_class):
+    def btn_connect(self): #이벤트 함수
+        global wificode
+        wificode = qrcode_wifi.start() 
+        print(wificode)      
+        if wificode is not None:
+            setwindow = Set_Window() 
+            widget.addWidget(setwindow) #1
+            widget.setCurrentIndex(widget.currentIndex()+1) # 다음화면으로 넘김 > qr내용 출력 화면
+        
+class Set_Window(QMainWindow, Qrtest_class):        
     def __init__(self):
+        global wificode
         super().__init__()
         self.setupUi(self)
-        self.setWindowTitle("Hoseo Unviersity - IR LAB")
-
+        self.setWindowTitle("Hoseo Unviersity - IR LAB")       
+        self.qr_label.setText(str(qr_result.result(wificode)))
+        self.qr_btn_yes.clicked.connect(lambda state, button=self.qr_btn_yes: self.btn_connect(state, button))
+        self.qr_btn_no.clicked.connect(lambda state, button=self.qr_btn_no: self.btn_connect(state, button))
+    
+    def btn_connect(self, state, button):
+        if button==self.qr_btn_yes:
+            qr_result.CreateWifiConfig(ssid, password)
+        elif button==self.qr_btn_no:
+            print('no버튼 클릭')
+            widget.setCurrentIndex(widget.currentIndex()-1) 
 
 if __name__ == "__main__":
+    
     app = QApplication(sys.argv)
     widget= QtWidgets.QStackedWidget()
 
-    mainwindow = MyWindow()
-    setwindow = Set_Window()
+    mainwindow = MyWindow() # 첫 화면
 
-    widget.addWidget(mainwindow)
-    widget.addWidget(setwindow)
-    widget.setFixedHeight(480)
-    widget.setFixedWidth(800)
+    widget.addWidget(mainwindow) #0
+    widget.setFixedHeight(480)  
+    widget.setFixedWidth(800)   
     widget.show()
 
     app.exec_()
