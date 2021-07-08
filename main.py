f55fd143c705933c41a151607f0ce92630f5ca33
kimsolbi <pinery11@naver.com>
2021-07-08 16:10:27 +0900

    .

--- main.py
+++ main.py
@@ -2,57 +2,65 @@ import sys
 import os
 
 import qrcode_wifi
-import Py_Json.to_json as tj
+import qr_result
+
 
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 from PyQt5 import QtWidgets
 
 #리소스 파일 사용시 resource_path()사용해서 절대경로로 변경
-def resource_path(relative_path):
-    try:
-        base_path = sys._MEIPASS
-    except Exception:
-        base_path = os.path.abspath(".")
 
-    return os.path.join(base_path, relative_path)
+set_class = uic.loadUiType('set.ui')[0]
+Qrtest_class = uic.loadUiType("camtest.ui")[0]
 
-
-set_class = uic.loadUiType(resource_path('set.ui'))[0]
-Qrtest_class = uic.loadUiType(resource_path("camtest.ui"))[0]
-
-
-class MyWindow(QMainWindow, set_class):
+wificode ='' # qr 내용 저장할 변수
+class MyWindow(QMainWindow, set_class): #set.ui *첫 화면     
     def __init__(self):
+        
         super().__init__()
-        self.setupUi(self)
-        self.setWindowTitle("Hoseo Unviersity - IR LAB")
+        self.setupUi(self) # 화면 나오게 함
+        self.setWindowTitle("Hoseo Unviersity - IR LAB") # 제목 표시줄
 
-        self.qrcode_btn.clicked.connect(self.btn_connect)
+        self.qrcode_btn.clicked.connect(self.btn_connect) # 이벤트 연결
     
-    def btn_connect(self):
-        wificode = qrcode_wifi.start()
+    def btn_connect(self): #이벤트 함수
+        global wificode
+        wificode = qrcode_wifi.start() 
+        print(wificode) #wificode를 다음 창에 넘겨줘야함       
         if wificode is not None:
-            tj.to_j(wificode)
-
-class Set_Window(QMainWindow, Qrtest_class):
+            setwindow = Set_Window() # qr인식
+            widget.addWidget(setwindow) #1
+            widget.setCurrentIndex(widget.currentIndex()+1) # 다음화면으로 넘김 > qr내용 출력 화면
+        
+class Set_Window(QMainWindow, Qrtest_class):    # camtest.ui *qr내용 출력됨    
     def __init__(self):
+        global wificode
         super().__init__()
         self.setupUi(self)
-        self.setWindowTitle("Hoseo Unviersity - IR LAB")
-
+        self.setWindowTitle("Hoseo Unviersity - IR LAB")       
+        self.qr_label.setText(str(qr_result.result(wificode)))
+        self.qr_btn_yes.clicked.connect(lambda state, button=self.qr_btn_yes: self.btn_connect(state, button))
+        self.qr_btn_no.clicked.connect(lambda state, button=self.qr_btn_no: self.btn_connect(state, button))
+    
+    def btn_connect(self, state, button):
+        if button==self.qr_btn_yes: #확인 이벤트
+            qr_result.test() 
+            #qr_result.CreateWifiConfig(ssid, password)
+        elif button==self.qr_btn_no: #취소 이벤트
+            print('no버튼 클릭')
+            widget.setCurrentIndex(widget.currentIndex()-1) 
 
 if __name__ == "__main__":
+    
     app = QApplication(sys.argv)
     widget= QtWidgets.QStackedWidget()
 
-    mainwindow = MyWindow()
-    setwindow = Set_Window()
+    mainwindow = MyWindow() # 첫 화면
 
-    widget.addWidget(mainwindow)
-    widget.addWidget(setwindow)
-    widget.setFixedHeight(480)
-    widget.setFixedWidth(800)
+    widget.addWidget(mainwindow) #0
+    widget.setFixedHeight(480)  #화면 크기(높이) 
+    widget.setFixedWidth(800)   #화면 크기(가로)
     widget.show()
 
     app.exec_()
--- qr_result.py
+++ qr_result.py
@@ -1,6 +1,7 @@
 import sys
 import os
 
+#qr정보 띄우는 함수
 def result(wificode): # 매개변수 ssid, group_code
     print("qr_result에서 출력"+wificode)
     print(type(wificode))
@@ -20,7 +21,8 @@ def result(wificode): # 매개변수 ssid, group_code
     print(r)
     
     return r
-
+'''     ***리눅스에서 돌리면 주석 제거하기***
+# wpa_supplicant.conf파일 수정하는 함수
 def CreateWifiConfig(SSID, password):
     config_lines = [
         '\n',
@@ -37,11 +39,11 @@ def CreateWifiConfig(SSID, password):
         wifi.write(config)
 
     print("Wifi config added")
-    os.system("sudo reboot now")
- #테스트 코드
-CreateWifiConfig("Ubiquitous_420", "1234567890")
+    os.system("sudo reboot now") # 재부팅하는 코드
+ 
+CreateWifiConfig("Ubiquitous_420", "1234567890") #테스트 코드
 
-'''    
+'''   
+#테스트 파일 
 def test():
     print('test 연결 완료')
-'''
