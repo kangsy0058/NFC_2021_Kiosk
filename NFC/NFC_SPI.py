081102550501afd6c7052400c3e622affb8dba90
Ji Jun Ho <sunny871@me.com>
2021-07-14 14:27:22 +0900

    NFC SPI 통신

---
+++ NFC/NFC_SPI.py
@@ -0,0 +1,36 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+"""
+This example shows connecting to the PN532 with I2C (requires clock
+stretching support), SPI, or UART. SPI is best, it uses the most pins but
+is the most reliable and universally supported.
+After initialization, try waving various 13.56MHz RFID cards over it!
+"""
+
+import board
+import busio
+from digitalio import DigitalInOut
+
+from adafruit_pn532.spi import PN532_SPI
+
+# SPI connection:
+spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+cs_pin = DigitalInOut(board.D5)
+pn532 = PN532_SPI(spi, cs_pin, debug=False)
+
+ic, ver, rev, support = pn532.firmware_version
+print("Found PN532 with firmware version: {0}.{1}".format(ver, rev))
+
+# Configure PN532 to communicate with MiFare cards
+pn532.SAM_configuration()
+
+print("Waiting for RFID/NFC card...")
+while True:
+    # Check if a card is available to read
+    uid = pn532.read_passive_target(timeout=0.5)
+    print(".", end="")
+    # Try again if no card is available.
+    if uid is None:
+        continue
+    print("Found card with UID:", [hex(i) for i in uid])
